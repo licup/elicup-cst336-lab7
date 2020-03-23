8d1d2e2f020a313a52c6fb32a8c57c9ffcc023d2
EC2 Default User <ec2-user@ip-172-31-41-113.us-east-2.compute.internal>
2020-03-23 06:18:32 +0000

    lab7

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.engine('html', require('ejs').renderFile);
 app.set("view engine", "ejs");
 app.use(express.static("public")); //folder for images, css, js
 
@@ -21,31 +22,27 @@ app.get("/results", async function(req, res){
     
     //console.dir(req);
     let keyword = req.query.keyword; //gets the value that the user typed in the form using the GET method
-    
-    let parsedData = await getImages(keyword);
+    let orient = req.query.orient;
+    let parsedData = await getImages(keyword, orient);
 
-    res.render("results", {"images":parsedData});
+    res.render("results", {"images":parsedData, "img":img});
     
 });//results route
 
 
 //Returns all data from the Pixabay API as JSON format
-function getImages(keyword){
+function getImages(keyword, orient){
     
     
     return new Promise( function(resolve, reject){
-        request('https://pixabay.com/api/?key=5589438-47a0bca778bf23fc2e8c5bf3e&q='+keyword,
+        request('https://pixabay.com/api/?key=5589438-47a0bca778bf23fc2e8c5bf3e&q='+keyword + "&orientation="+orient,
                  function (error, response, body) {
     
             if (!error && response.statusCode == 200  ) { //no issues in the request
                 
-                 let parsedData = JSON.parse(body); //converts string to JSON
-                 
-                 resolve(parsedData);
-                
-                //let randomIndex = Math.floor(Math.random() * parsedData.hits.length);
-                //res.send(`<img src='${parsedData.hits[randomIndex].largeImageURL}'>`);
-                //res.render("index", {"image":parsedData.hits[randomIndex].largeImageURL});
+                let parsedData = JSON.parse(body); //converts string to JSON
+                resolve(parsedData);
+                console.log(parsedData.hits[0].likes);
                 
             } else {
                 reject(error);
@@ -59,6 +56,16 @@ function getImages(keyword){
     
 }
 
+function Random(res){
+    var arr=[];
+    while (arr.length!=4){
+    let randomIndex = Math.floor(Math.random() * res.hits.length);
+        if(arr.indexOf(randomIndex)==-1){
+            arr.push(randomIndex);
+        }
+    }
+    return arr;
+}
 
 //starting server
 app.listen(process.env.PORT, process.env.IP, function(){
